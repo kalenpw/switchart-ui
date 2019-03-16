924c4ec96120820914630eee7d72459fb360a148
kalenpw <kalenpwilliams@gmail.com>
2019-03-15 21:37:57 -0600

    Sticky footer

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,30 @@
         <navbar></navbar>
         <div class="section">
             <router-view/>
-            <FlashMessage></FlashMessage>
         </div>
+        <End></End>
     </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
-import FlashMessage from "@/components/FlashMessage.vue";
+import End from "@/components/End.vue";
 export default {
     name: "app",
     components: {
         Navbar,
-        FlashMessage
+        End
     }
 };
 </script>
 
-<style>
+<style scoped>
+#app{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+}
+.section{
+    flex: 1;
+}
 </style>
---
+++ src/components/End.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <FlashMessage></FlashMessage>
+        <section class="hero is-dark">
+            <div class="hero-body">
+                <div class="container"></div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import FlashMessage from "@/components/FlashMessage.vue";
+
+export default {
+    name: "End",
+    components: {
+        FlashMessage
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+    created() {}
+};
+</script>
+
+<style>
+</style>
